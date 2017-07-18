a0d01239c85b7967efd479d52777d631aa5a3e79
ilsecentenoz <centenoilse93@gmail.com>
2017-07-18 13:47:56 +0000

    firts commit

--- public/js/baba.js/app.js
+++
@@ -1,1 +0,0 @@
-alert("gatito escupido"),alert("gatito escupido"),alert("gatito escupido"),alert("gatito escupido"),alert("gatito escupido"),alert("amarillo"),alert("me cagado en tu madre");var malteada=function(){console.log("hola malteada")},malteadaChocolATE=function(){console.log("hola malteada CHOCOLATE n.l.n")};
